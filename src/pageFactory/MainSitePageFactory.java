1f75eac4e8a76be536366d954765781d88f4d993
Steven983 <StefanRanoszek@protonmail.ch>
2019-03-27 11:37:27 +0000

    Update MainSitePageFactory.java

--- src/pageFactory/MainSitePageFactory.java
+++ src/pageFactory/MainSitePageFactory.java
@@ -79,7 +79,7 @@ public class MainSitePageFactory {
         }
         public void setLogin(String strLogin){
 
-            t.sendKeys(strLogin);
+            accept.sendKeys(strLogin);
         }
 
 
